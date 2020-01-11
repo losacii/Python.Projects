2488acaff0fd923cb3891c61333dccd5c604e896
apple <apple@appledeMac-mini.local>
2020-01-11 09:31:28 +0800

    "update"

--- 01_pyshopping/products/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse('hello there!')
-
-def about(request):
-    return HttpResponse('about page')
--- try_Django/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-# Register your models here.
-
-# Register 'Post' to admin-page
-# in blog/admin.py, import Post, then register it.
-admin.site.register(Post)
-
